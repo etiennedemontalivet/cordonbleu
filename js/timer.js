0d5452ed05cb43a014f72dc29a089eedc88c4594
etienne <etienne.demontalivet@gmail.com>
2017-07-27 11:28:27 +0200

    Fix counters issues.

--- js/timer.js
+++ js/timer.js
@@ -1,2 +1,5 @@
 
-$('.statistic-counter').counterUp();
+$('#counter1').counterUp("63", 1000);
+$('#counter2').counterUp("59", 1000);
+$('#counter3').counterUp("56", 1000);
+$('#counter4').counterUp("54", 1000);

69c96709909eca83bfd1b9c66e12ae02f38fccca
etienne <etienne.demontalivet@gmail.com>
2017-07-27 10:03:50 +0200

    Add loading page and resize margins of team user.

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+$('#headerId').hide();
+$('#loading').css('height',$(window).height());
+$('#loading').css('width',$(window).width());
+
+$(window).load(function() {
+	$('#loading').hide();
+	$('#headerId').show();
+});
